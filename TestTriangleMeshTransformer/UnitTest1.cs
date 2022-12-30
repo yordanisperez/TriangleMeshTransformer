ef1f1a158451fe8098d6dde66678085eff739987
Yordanis Perez Brito <yordanis@gmail.com>
2022-12-30 14:00:33 -0500

    Desing main view

---
+++ TestTriangleMeshTransformer/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using Moq;
+using System.Windows;
+using TriangleMeshTransformer;
+
+
+namespace TestTriangleMeshTransformer
+{
+    
+    [TestClass]
+    public class MainWindowsTest
+    {
+        [TestMethod]
+        public void testMiOpen_Click()
+        {
+            
+            // Create a mock of the FileDialog
+            var fileDialogMock = new Mock<IOpenFileDialog>();
+            // Asigna un controlador de eventos al botón que llama al OpenDialog
+            // Set up the mock to return a specific file path when ShowDialog() is called
+            fileDialogMock.Setup(x => x.ShowDialog()).Returns(true);
+            fileDialogMock.Setup(x => x.InitialDirectory).Returns("");
+            fileDialogMock.Setup(x => x.Filter).Returns("");
+            fileDialogMock.SetupGet(x => x.FileName).Returns(@"C:\myfile.txt");
+           // fileDialogMock.SetupGet(x => x.FileNames).Returns(@"C:\myfile.txt");
+            // Create an instance of the MainWindow class
+            MainWindow mainWindow = new MainWindow();
+
+            // Set the FileDialog property to the mock
+            mainWindow.openFileDialog = (IOpenFileDialog) fileDialogMock.Object;
+
+
+            // Simulate the OpenFileButton_Click event
+            mainWindow.miOpen_Click(new object(), new RoutedEventArgs());
+
+            // Verify that the file path returned by the mock was used to set the FilePath property
+            Assert.AreEqual(@"C:\myfile.txt", mainWindow.filePathOpen);
+
+        }
+    }
+}
--- TriangleMeshTransformer/MainWindow.xaml.cs
+++ TriangleMeshTransformer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,13 +17,88 @@ using System.Windows.Shapes;
 namespace TriangleMeshTransformer
 {
     /// <summary>
+    /// This class is for can mock to OpenDialog from MSTest
+    /// </summary>
+    public interface IOpenFileDialog
+    {
+        string Filter { get; set; }
+        bool? ShowDialog();
+        string FileName { get; set; }
+        string InitialDirectory { get; set; }
+    }
+    /// <summary>
+    /// Class container wrapper for OpenFileDialog need for testing mock MSTest
+    /// </summary>
+    public class OpenFileDialogContainer : IOpenFileDialog
+    {
+       public OpenFileDialog openFileDialog { get; set; }
+        public string Filter
+        {
+            get => openFileDialog.Filter; set
+            {
+                openFileDialog.Filter =value;
+            }
+        }
+        public string FileName { 
+            get => openFileDialog.FileName; set  {
+                openFileDialog.FileName = value;
+            } 
+        }
+        public string InitialDirectory { 
+            get => openFileDialog.InitialDirectory; set {
+               openFileDialog.InitialDirectory=value;
+            }
+        }
+
+        public OpenFileDialogContainer(OpenFileDialog pOpenfile)
+        {
+            openFileDialog=pOpenfile;
+        }
+
+        public bool? ShowDialog()
+        {
+            return openFileDialog.ShowDialog();
+        }
+    }
+
+    /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+      public  string filePathOpen=@"C:\";
+      public IOpenFileDialog openFileDialog;
         public MainWindow()
         {
             InitializeComponent();
+            // Create an instance of the OpenFileDialog
+            openFileDialog = new OpenFileDialogContainer(new OpenFileDialog()) ;
+
+        }
+
+        public void miOpen_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Set the filter for the file extension
+            openFileDialog.Filter = "Text files (*.stl)|*.stl";
+
+            // Find the position of the delimiter in the original string
+            int index = filePathOpen.IndexOf(@"\");
+            // Extract the substring before the delimiter
+            // Set the initial directory
+            openFileDialog.InitialDirectory = filePathOpen.Substring(0, index);
+
+            // Show the OpenFileDialog and get the result
+            bool? result = openFileDialog.ShowDialog();
+
+            // If the user selected a file and clicked "Open"
+            if (result == true)
+            {
+                // Get the path of the selected file
+                filePathOpen = openFileDialog.FileName; 
+                //LoadingFile and create Structure mesh
+               // MessageBox.Show(filePathOpen);
+            }
         }
     }
 }
